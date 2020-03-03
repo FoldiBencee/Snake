b899bf06c7740816fd1626a28694f8ceb3bff739
foldibence <foldib555@gmail.com>
2020-03-03 18:40:37 +0100

    jatek vendegkent funkcio

--- app/src/main/java/com/example/snake/MainActivity.java
+++ app/src/main/java/com/example/snake/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.snake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonbelepes,buttonregisztracio;
+    private TextView vendeg;
 
 
 
@@ -40,6 +44,32 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        vendeg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle("Folytatás mint vendég");
+                alertDialog.setMessage("Ha biztosan vendégként szeretne játszani, akkor a pontszámaid nem lesznek elmentve, és nem lesz megjelenítve a globális ranglistán");
+                alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent menube = new Intent(MainActivity.this, fomenu4Activity.class);
+                        startActivity(menube);
+                        finish();
+                    }
+                });
+
+                alertDialog.setNegativeButton("vissza", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                alertDialog.create().show();
+
+            }
+        });
+
 
 
     }
@@ -50,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
     {
         buttonbelepes = findViewById(R.id.idbttnBelepes);
         buttonregisztracio = findViewById(R.id.idbttnRegisztracio);
+        vendeg = findViewById(R.id.txtvendeg);
     }
 
 
